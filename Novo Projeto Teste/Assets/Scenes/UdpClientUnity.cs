183860c16ef9d3d0905d1009787d4fcc26652178
lvinia <145567312+lvinia@users.noreply.github.com>
2025-09-10 08:20:26 -0300

    REDES

---
+++ Novo Projeto Teste/Assets/Scenes/UdpClientUnity.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+
+using System.Net.Sockets;
+
+using System.Text;
+
+public class UdpClientUnity : MonoBehaviour 
+{
+
+UdpClient client;
+
+void Start() {
+
+client = new UdpClient();
+
+client.Connect("10.57.10.16", 5000);
+
+Debug.Log("Cliente conectado ao servidor");
+
+}
+
+void Update() {
+
+if
+(Input.GetKeyDown(KeyCode.Space)) {
+
+string msg = "Olá do cliente Unity!";
+
+byte[] data =
+Encoding.UTF8.GetBytes(msg);
+
+client.Send(data, data.Length);
+
+Debug.Log("Mensagem enviada: " +
+msg);
+
+}
+
+}
+
+}
